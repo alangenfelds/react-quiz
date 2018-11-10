f1c46de562d37b6d2b7a900fcf2b541916010524
Langenfelds, Arturs <a.langenfelds@a1qa.com>
2018-11-10 08:36:30 +0200

    added backdrop

--- src/components/Navigation/Drawer/Drawer.js
+++ src/components/Navigation/Drawer/Drawer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 
 import classes from "./Drawer.css";
+import Backdrop from '../../UI/Backdrop/Backdrop';
 
 const links = [1,2,3];
 
@@ -24,11 +25,14 @@ class Drawer extends Component {
     ]
 
     return (
-        <nav className={cls.join(' ')}>
-            <ul>
-                { this.renderLinks() }
-            </ul>
-        </nav>
+        <React.Fragment>
+            <nav className={cls.join(' ')}>
+                <ul>
+                    { this.renderLinks() }
+                </ul>
+            </nav>
+            {this.props.isOpen ? <Backdrop onClick={this.props.onClose} /> : null}
+        </React.Fragment>
     )
 
   }
